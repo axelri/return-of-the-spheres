9ad3fcb3dfb9ddce370489ab5a9df6f4c1100e15
axelri <axel.riese@gmail.com>
2013-03-13 15:32:39 +0100

    Vector overhaul, several more complete test cases.

--- vector.py
+++ vector.py
@@ -1,157 +1,136 @@
 from math import *
 import numbers
+
 class Vector:
     ''' A vector class to simplify calculations on vectors '''
     
     def __init__(self, value = [0.0, 0.0, 0.0]):
         # Shorter calls for vectors we use a lot
-        assert isinstance(value, str) or isinstance(value, list), \
-                'Input must be either a string or a list'
+        # NOTE: Removed. Maybe the gain doesn't justify the confusion?
+        assert isinstance(value, list), \
+                'Input must be a list'
 
         if __debug__:
-            if isinstance(value, str):
-                assert value == 'e_x' or value == 'e_y' or value == 'e_z', \
-                        'Input string must be a unit vector.'
-            if isinstance(value, list):
-                for number in value:
-                    assert isinstance(number, numbers.Number), \
+            for number in value:
+                assert isinstance(number, numbers.Number), \
                     'Every component must be a number.'
 
-        if value == 'e_x':
-            self._value = [1.0, 0.0, 0.0]
-        elif value == 'e_y':
-            self._value = [0.0, 1.0, 0.0]
-        elif value == 'e_z':
-            self._value = [0.0, 0.0, 1.0]
-        else:
-            self._value = value
+        self.value = value
 
-    def get_value(self):
-        return self._value
+    def dim(self):
+        return len(self.value)
 
-    def set_value(self, value):
-        self._value = value
-    
+    def is_zero(self):
+        return self.value == [0] * self.dim()
+
+    def is_not_zero(self):
+        return self.value != [0] * self.dim()
+
     def dot(self, v2):
         ''' Calculates the dot product of the vector and v2 '''
         assert isinstance(v2, Vector), 'Input must be a vector'
-        assert len(self.get_value() == v2.get_value()), \
-                'Vectors must be of the same length'
+        assert self.dim() ==  v2.dim(), \
+                'Vectors must be of the same dimension'
 
         tot = 0
-        for i in range(len(self._value)):
-            tot += self._value[i] * v2._value[i]
+        for i in range(len(self.value)):
+            tot += self.value[i] * v2.value[i]
         return tot
 
-    def cross(self, V2):
+    def cross(self, v):
         ''' Calculates the cross product of the vector and another vector V2.
             Note that it is calculated as (self x V2), since the cross product
             isn't commutative '''
-        assert isinstance(V2, Vector), 'Input must be a vector'
-        assert len(self.get_value()) == 3 and  len(v2.get_value()) == 3, \
+        assert isinstance(v, Vector), 'Input must be a vector'
+        assert self.dim() == 3 and v.dim() == 3, \
                 'Cross product is only defined in 3 dimensions.'
 
-        v1 = self._value
-        v2 = V2._value
-        out = [v1[1] * v2[2] - v1[2] * v2[1],
-               v1[2] * v2[0] - v1[0] * v2[2],
-               v1[0] * v2[1] - v1[1] * v2[0]]
-        return Vector(out)
+        v1 = self.value
+        v2 = v.value
+        return Vector([v1[1] * v2[2] - v1[2] * v2[1],
+                       v1[2] * v2[0] - v1[0] * v2[2],
+                       v1[0] * v2[1] - v1[1] * v2[0]])
 
     def norm(self):
         ''' Calculates the norm of the vector '''
-        return (self.dot(self))**0.5
+        return sqrt((self.dot(self)))
 
-    def normalize(self):
-        ''' Normalizes the vector. Returns None if the vector
-        is the zero vector.'''
-        n = self.norm()
-        v = self._value
-        if n != 0:
-            for i in range(len(v)):
-                v[i] /= n
-        else:
+    def normalized(self):
+        ''' Returns a normaliszed version of the the vector. 
+        Returns None if the vector is the zero vector.'''
+        if self.is_zero():
             return None
-        return Vector(v)
 
-    def __mul__(self, scalar):
-        ''' Multiplies the vector with the scalar '''
-        assert isinstance(scalar, numbers.Number), \
-            'Input must be a real number.' 
-        out = [0]*len(self._value)
-        for i in range(len(self._value)):
-            out[i] = self._value[i] * scalar
+        n = self.norm()
+        out = []
+        for component in self.value:
+            out.append(component / n)
         return Vector(out)
 
-    def __add__(self, v2):
-        ''' Adds the vector to v2 '''
-        assert isinstance(v2, Vector), 'Input must be a vector.'
-        assert len(self.get_value() == v2.get_value()), \
-                'Vectors must be of the same length'
+    def __eq__(self, v2):
+        ''' Checks if two vectors have the same value, overloads "==" '''
+        assert isinstance(v2, Vector), 'Input must be a vector'
+        return self.value == v2.value
 
-        out = [0]*len(self._value)
-        for i in range(len(self._value)):
-            out[i] = self._value[i] + v2._value[i]
+    def __ne__(self, v2):
+        ''' Checks if two vectors do not have the same value, overloads "!=" '''
+        assert isinstance(v2, Vector), 'Input must be a vector'
+        return self.value != v2.value
+
+    def __mul__(self, scalar):
+        ''' Returns the vector multiplied with the given scalar '''
+        assert isinstance(scalar, numbers.Number), \
+            'Input must be a real number.' 
+
+        out = []
+        for component in self.value:
+            out.append(component * scalar)
+        return Vector(out)
+
+    def __neg__(self):
+        '''Returns the vector in negative direction.'''
+        return self * -1
+
+    def __add__(self, v2):
+        ''' Returns the vector added with the given other vector.'''
+        assert isinstance(v2, Vector), 'Input must be a vector.'
+        assert self.dim() == v2.dim(), \
+                'Vectors must be of the same dimension'
+
+        out = []
+        for comp1, comp2 in zip(self.value, v2.value):
+            out.append(comp1 + comp2)
         return Vector(out)
 
     def __sub__(self, v2):
         assert isinstance(v2, Vector), 'Input must be a vector.'
-        assert len(self.get_value() == v2.get_value()), \
-                'Vectors must be of the same length'
+        assert self.dim() == v2.dim(), \
+                'Vectors must be of the same dimension'
 
-        return self.__add__(v2.__mul__(-1.0))
+        return self + (-v2)
 
-    # TODO: Make projection a single function with varargs
-    # TODO: Write more assertions
-    def proj_norm(self, v2):
-        ''' Returns the norm of the projection of the vector 
-            on the vector v2 '''
-        assert isinstance(v2, Vector), 'Input must be a vector.'
-        assert len(self.get_value() == v2.get_value()), \
-                'Vectors must be of the same length'
+    def projected(self, *base):
+        ''' Returns the projection of the vector on the vector space 
+        defined by the supplied vectors. Assumes orthogonal and linearly
+        independant base vectors.'''
+        if __debug__:
+            for v in base:
+                assert isinstance(v, Vector), 'Input must be a vector.'
+                assert self.dim() == v.dim(), 'Vectors must be of the same dimension'
+                assert v.is_not_zero, "Input can't be the the zero vector"
 
-        if v2.norm() != 0:
-            e2 = v2.normalize()
-            return self.dot(e2)
-        else:
-            return 0
+        proj = Vector([0] * self.dim())
+        for v in base:
+            base_proj = v * (float(self.dot(v))/float(v.dot(v)))
+            proj += base_proj
 
-    def projection(self, v2):
-        ''' Returns the projection of the vector on the vector v2 '''
-        assert isinstance(v2, Vector), 'Input must be a vector.'
-        n = self.proj_norm(v2)
-        e2 = v2.normalize()
-        if e2 != None:
-            out = e2.__mul__(n)
-            return out
-        else:
-            return Vector([0]*len(self._value)) # A zero vector
-
-    def proj_syst(self, e1, e2, e3):
-        ''' Returns the projection of the vector in the coordinate system
-            defined by the vectors e1, e2 and e3 (e1, e2 and e3 must be
-            orthogonal'''
-        proj1 = self.projection(e1)
-        proj2 = self.projection(e2)
-        proj3 = self.projection(e3)
-        out = proj1.v_add(proj2.v_add(proj3)) # Adds all vectors
-        
-        return out
-
-    def proj_plane(self, e1, e2):
-        ''' Returns the projection of the vector on the plane defined
-            by the vectors e1 and e2 (e1 and e2 must be orthogonal) '''
-        proj1 = self.projection(e1)
-        proj2 = self.projection(e2)
-        out = proj1.v_add(proj2)
-
-        return out
+        return proj
 
     def distance_vector(self, point):
         ''' Returns a vector from self to point '''
         # Rather unnessecary, why not just use point.v_add(self.v_mult(-1.0))?
-        p = point.get_value()
-        s = self._value
+        p = point.value
+        s = self.value
         return Vector([p[0] - s[0],
                        p[1] - s[1],
                        p[2] - s[2]])
@@ -173,3 +152,7 @@ class Vector:
         term2 = v2.v_mult(self.dot(v3))
         out = term1.v_add(term2)
         return out
+
+    def __str__(self):
+        ''' Returns the string representation of the vector.'''
+        return self.value.__str__()
--- vector_test.py
+++ vector_test.py
@@ -1,4 +1,5 @@
 import unittest
+import math
 from vector import *
 
 class TestVector(unittest.TestCase):
@@ -6,17 +7,65 @@ class TestVector(unittest.TestCase):
     def setUp(self):
         self.v1 = Vector([2, 0, 3])
         self.v2 = Vector([0, 2, 3])
-        self.v3 = Vector([1, 3, 0])
+        self.v3 = Vector([1, 3, -2])
 
-    def test_proj(self):
-        pr = [0, 18.0/13.0, 27.0/13.0]
-        proj = self.v1.projection(self.v2).get_value()
-        for i in range(len(pr)):
-            self.assertAlmostEqual(pr[i], proj[i])
+    def test_dim(self):
+        self.assertEqual(3, self.v1.dim())
+        self.assertEqual(4, Vector([1, 2, 3, 4]).dim())
 
-    def check_equal(self, v1, v2):
-        a = v1.get_value();
-        b = v2.get_value();
+    def test_is_zero(self):
+        self.assertFalse(self.v1.is_zero())
+        self.assertTrue(Vector([0, 0, 0]).is_zero())
+
+    def test_is_not_zero(self):
+        self.assertTrue(self.v1.is_not_zero())
+        self.assertFalse(Vector([0, 0, 0]).is_not_zero())
+
+    def test_dot(self):
+        self.assertEqual(9, self.v1.dot(self.v2))
+
+    def test_cross(self):
+            self.assertEqual([-6, -6, 4], self.v1.cross(self.v2).value)
+
+    def test_project(self):
+        expected = [0, 18.0/13.0, 27.0/13.0]
+        actual = self.v1.projected(self.v2).value
+
+        for a, b in zip(expected, actual):
+            self.assertAlmostEqual(a, b)
+
+        expected = [-2.0/7.0, 18.0/13.0 - 6.0/7.0, 27.0/13.0 + 4.0/7.0]
+        actual = self.v1.projected(self.v2, self.v3).value
+
+        for a, b in zip(expected, actual):
+            self.assertAlmostEqual(a, b)
+
+    def test_norm(self):
+        self.assertAlmostEqual(math.sqrt(13), self.v1.norm())
+
+    def test_normalized(self):
+        v = Vector(self.v1.value)
+        self.assertAlmostEqual(1, self.v1.normalized().norm())
+        self.assertEqual(v, self.v1) # should not mutate
+
+    def test_equal(self):
+        self.assertEqual(self.v1, Vector([2, 0, 3]))
+
+    def test_not_equal(self):
+        self.assertNotEqual(self.v1, self.v2)
+    
+    def test_mult_scalar(self):
+        self.assertEqual([4, 0 , 6], (self.v1 * 2).value)
+
+    def test_unary_neg(self):
+        self.assertEqual([-2, 0, -3], (-self.v1).value)
+
+    def test_vector_addition(self):
+        self.assertEqual([2, 2, 6], (self.v1 + self.v2).value)
+
+    def test_vector_subtraction(self):
+        self.assertEqual([2, -2, 0], (self.v1 - self.v2).value)
+
 
 if __name__ == '__main__':
     unittest.main()
