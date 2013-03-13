d9b014411a4ce2edb64595f8b48aa183521cce2d
axelri <axel.riese@gmail.com>
2013-03-13 17:32:00 +0100

    Config file, some code base

---
+++ rots.py
@@ -0,0 +1,16 @@
+import traceback
+import sys
+
+def main():
+    ''' Main routine of the game.'''
+
+    while true:
+        # do main routine
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception:
+        traceback.print_exc(file=sys.stdout)
+    finally:
+        pygame.quit()
