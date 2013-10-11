ddcb30e47c2d08710cb6a80ba356d1b0a4879f45
martin-bjork <martin.bjork_92@hotmail.com>
2013-10-12 00:57:59 +0200

    Yet another try to implement shadows, to no avail...

--- rots/graphics/init_graphics.py
+++ rots/graphics/init_graphics.py
@@ -7,7 +7,7 @@ from OpenGL.GLUT import *
 import itertools
 from math import tan, pi, radians
 
-from graphics import textures, draw
+from graphics import textures, draw, lights
 from objects.text import TextBox
 
 def init_window(window_name, HAVE_FULLSCREEN = True):
@@ -83,126 +83,79 @@ def init_window(window_name, HAVE_FULLSCREEN = True):
     glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST)
     glHint(GL_POLYGON_SMOOTH_HINT, GL_NICEST)
     glHint(GL_LINE_SMOOTH_HINT, GL_NICEST)
+    glHint(GL_POINT_SMOOTH_HINT, GL_NICEST)
+
+    # Initialize shadows
+
+    #shadow_map_size = 512
+    #lights.init_shadows([[shadow_map_size, 5]])
 
     return width, height, aspect_angle
 
-def init_shadows(game):
-    ''' Sets the necessary constants for shadow mapping. '''
+# def init_shadows(game):
+#     ''' Sets the necessary constants for shadow mapping. '''
 
-    shadow_map_size = 512
-    window_width = pygame.display.Info().current_w
-    window_height = pygame.display.Info().current_h
+#     shadow_map_size = 512
+#     window_width = pygame.display.Info().current_w
+#     window_height = pygame.display.Info().current_h
 
-    # Create the shadow map texture
-    shadow_map_texture = glGenTextures(1)
-    glBindTexture(GL_TEXTURE_2D, shadow_map_texture)
-    glTexImage2D( GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, shadow_map_size, shadow_map_size, 0,
-        GL_DEPTH_COMPONENT, GL_UNSIGNED_BYTE, None)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP)
+#     # Create the shadow map texture
+#     shadow_map_texture = glGenTextures(1)
+#     glBindTexture(GL_TEXTURE_2D, shadow_map_texture)
+#     glTexImage2D( GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, shadow_map_size, shadow_map_size, 0,
+#         GL_DEPTH_COMPONENT, GL_UNSIGNED_BYTE, None)
+#     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+#     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+#     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP)
+#     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP)
 
-    # Calculate and save matrices
-    glMatrixMode(GL_MODELVIEW)
-    glPushMatrix()
+#     # Calculate and save matrices
+#     glMatrixMode(GL_MODELVIEW)
+#     glPushMatrix()
 
-    glLoadIdentity()
-    gluPerspective(45.0, float(window_width) / float(window_height), 1.0, 100.0)
-    camera_projection_matrix = glGetFloatv(GL_MODELVIEW_MATRIX)
+#     glLoadIdentity()
+#     gluPerspective(45.0, float(window_width) / float(window_height), 1.0, 100.0)
+#     camera_projection_matrix = glGetFloatv(GL_MODELVIEW_MATRIX)
 
-    glLoadIdentity()
-    game._camera.view(game._player)
-    camera_view_matrix = glGetFloatv(GL_MODELVIEW_MATRIX)
+#     glLoadIdentity()
+#     game._camera.view(game._player)
+#     camera_view_matrix = glGetFloatv(GL_MODELVIEW_MATRIX)
 
-    glLoadIdentity()
-    gluPerspective(45.0, 1.0, 2.0, 8.0)
-    light_projection_matrix_c = glGetFloatv(GL_MODELVIEW_MATRIX)
-    light_proj_list = [list(value) for value in list(light_projection_matrix_c)]
-    merged = list(itertools.chain.from_iterable(light_proj_list))
-    light_projection_matrix = merged
+#     glLoadIdentity()
+#     gluPerspective(45.0, 1.0, 2.0, 8.0)
+#     light_projection_matrix_c = glGetFloatv(GL_MODELVIEW_MATRIX)
+#     light_proj_list = [list(value) for value in list(light_projection_matrix_c)]
+#     merged = list(itertools.chain.from_iterable(light_proj_list))
+#     light_projection_matrix = merged
 
-    light_view_matrix_list = []
-    for light in game.get_light_list():
-        light_pos = light.get_pos().value
-        glLoadIdentity()
-        gluLookAt( light_pos[0], light_pos[1], light_pos[2],
-        0.0, 0.0, 0.0,
-        0.0, 1.0, 0.0)
-        light_view_matrix_c = glGetFloatv(GL_MODELVIEW_MATRIX)
+#     light_view_matrix_list = []
+#     for light in game.get_light_list():
+#         light_pos = light.get_pos().value
+#         glLoadIdentity()
+#         gluLookAt( light_pos[0], light_pos[1], light_pos[2],
+#         0.0, 0.0, 0.0,
+#         0.0, 1.0, 0.0)
+#         light_view_matrix_c = glGetFloatv(GL_MODELVIEW_MATRIX)
         
-        light_view_list = [list(value) for value in list(light_view_matrix_c)]
-        merged = list(itertools.chain.from_iterable(light_view_list))
-        light_view_matrix = merged
-        light_view_matrix_list.append(light_view_matrix)
+#         light_view_list = [list(value) for value in list(light_view_matrix_c)]
+#         merged = list(itertools.chain.from_iterable(light_view_list))
+#         light_view_matrix = merged
+#         light_view_matrix_list.append(light_view_matrix)
 
-    glPopMatrix()
+#     glPopMatrix()
 
-    bias_matrix = (0.5, 0.0, 0.0, 0.0,
-                    0.0, 0.5, 0.0, 0.0,
-                    0.0, 0.0, 0.5, 0.0,
-                    0.5, 0.5, 0.5, 1.0)
+#     bias_matrix = (0.5, 0.0, 0.0, 0.0,
+#                     0.0, 0.5, 0.0, 0.0,
+#                     0.0, 0.0, 0.5, 0.0,
+#                     0.5, 0.5, 0.5, 1.0)
 
-    # Save the matrices as game constants
-    game.add_constant('shadow_map_size', shadow_map_size)
-    game.add_constant('window_width', window_width)
-    game.add_constant('window_height', window_height)
-    game.add_constant('shadow_map_texture', shadow_map_texture)
-    game.add_constant('camera_projection_matrix', camera_projection_matrix)
-    game.add_constant('camera_view_matrix', camera_view_matrix)
-    game.add_constant('light_projection_matrix', light_projection_matrix)
-    game.add_constant('light_view_matrix_list', light_view_matrix_list)
-    game.add_constant('bias_matrix', bias_matrix)
-
-
-# TODO: Make this a 'Loading-screen' class instead
-
-class Start_screen:
-    ''' A class for the start screen. Is used to show
-        an image and messages during the loading of the game. '''
-
-    def __init__(self, start_image, width, height, aspect_angle):
-        ''' Initializes the start screen. Creates a quad
-            to show the background image on and a textbox
-            to show messages on. '''
-
-        # Set up the perspective
-        self._ratio = float(width)/float(height)
-        self._distance = tan(radians(90 - aspect_angle/2.0 ))
-
-        # Create a display list for the start screen image
-        start_texture = textures.load_texture(start_image)
-
-        display_list_index = glGenLists(1)
-        glNewList(display_list_index, GL_COMPILE)
-        draw.start_screen(start_texture, self._ratio)
-        glEndList()
-
-        self._display_list_index = display_list_index
-
-        # Create a text box
-        self._text_box = TextBox('test.ttf', 40, width/2 - 200, 
-                                        height/2, [1,0,0])
-
-    def update(self, message):
-        ''' Updates and draws the start screen '''
-
-        glDisable(GL_LIGHTING)
-
-        self._text_box.set_string(message)
-
-        glPushMatrix()
-        glLoadIdentity()
-        gluLookAt(0.0, 0.0, self._distance,
-                    0.0, 0.0, 0.0,
-                    0.0, 1.0, 0.0)
-
-        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
-        
-        glCallList(self._display_list_index)
-
-        self._text_box.draw()
-
-        glPopMatrix()
-        pygame.display.flip()
-        glEnable(GL_LIGHTING)
+#     # Save the matrices as game constants
+#     game.add_constant('shadow_map_size', shadow_map_size)
+#     game.add_constant('window_width', window_width)
+#     game.add_constant('window_height', window_height)
+#     game.add_constant('shadow_map_texture', shadow_map_texture)
+#     game.add_constant('camera_projection_matrix', camera_projection_matrix)
+#     game.add_constant('camera_view_matrix', camera_view_matrix)
+#     game.add_constant('light_projection_matrix', light_projection_matrix)
+#     game.add_constant('light_view_matrix_list', light_view_matrix_list)
+#     game.add_constant('bias_matrix', bias_matrix)
--- rots/graphics/lights.py
+++ rots/graphics/lights.py
@@ -1,4 +1,8 @@
 from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GL.ARB.shadow import *
+from OpenGL.GL.ARB.depth_texture import *
+from OpenGL.GL.ARB.transpose_matrix import *
 from math_classes.vectors import Vector
 
 # TODO: Add other attributes to the light, such as color, direction,
@@ -11,7 +15,8 @@ class Light:
     def __init__(self, light_index, pos, is_spotlight = False,
                  ambient = [0.2, 0.2, 0.2, 1.0],
                  diffuse = [0.8, 0.8, 0.8, 1.0],
-                 specular = [0.5, 0.5, 0.5, 1.0]):
+                 specular = [0.5, 0.5, 0.5, 1.0],
+                 shadow_color = [0.2, 0.2, 0.2, 1.0]):
         ''' Initializes the Light object, sets the OpenGL-index
         of the light and its starting position.
 
@@ -25,6 +30,7 @@ class Light:
         self._ambient = ambient
         self._diffuse = diffuse
         self._specular = specular
+        self._shadow_color = shadow_color
 
         glEnable(self._light_index)
 
@@ -51,6 +57,19 @@ class Light:
     def disable(self):
         ''' Disables the light '''
         glDisable(self._light_index)
+
+    def enable(self):
+        glEnable(self._light_index)
+
+    def change_diffuse_color(self,newcolor):
+        self.diffuse_color = newcolor
+        glLightfv(self._light_index,GL_AMBIENT,[0.0,0.0,0.0,1.0])
+        glLightfv(self._light_index,GL_DIFFUSE,[self.diffuse_color[0], 
+                    self.diffuse_color[1],self.diffuse_color[2],1.0])
+    def change_color(self,newcolor):
+        self.color = newcolor
+        glLightfv(self._light_index,GL_SPECULAR,[self.color[0],self.color[1],
+                            self.color[2],1.0])
 
     def draw_pos(self):
         ''' Shows the position and color of the light by drawing 
@@ -121,3 +140,173 @@ class Light:
     def set_specular(self, specular):
         self._specular = specular
         self._setup()
+
+# The code for shadow rendering is almost entirely copied from
+# http://geometrian.com/programming/projects/index.php?project=glLib
+GLLIB_SHADOW_MAP1 = 5
+GLLIB_SHADOW_MAP2 = 6
+GLLIB_SHADOW_MAP3 = 7
+GLLIB_SHADOW_MAP4 = 8
+GLLIB_SHADOW_MAP5 = 9
+GLLIB_SHADOW_MAP6 = 10
+GLLIB_SHADOW_MAP7 = 11
+GLLIB_SHADOW_MAP8 = 12
+
+def init_shadows(shadowmaps=[[256,0]]):
+    global ShadowMaps
+    ShadowMaps = []
+    for newmap in shadowmaps:
+        ShadowMap = resize_shadowmap(newmap[0],newmap[1])
+        ShadowMaps.append([ShadowMap,newmap[0],None])
+
+def resize_shadowmap(newresolution,antialiaslevel=0):
+    texturing = glGetBooleanv(GL_TEXTURE_2D)
+    glEnable(GL_TEXTURE_2D)
+    ShadowMapSize = newresolution
+    mods = ["depthtex","clamp"]
+    if   antialiaslevel == 0: pass
+    elif antialiaslevel == 1: mods.append("filter")
+    elif antialiaslevel == 2: mods.append("mipmap")
+    elif antialiaslevel == 3: mods.append("mipmap"); mods.append("filter")
+    elif antialiaslevel == 4: mods.append("mipmap blend")
+    elif antialiaslevel == 5: mods.append("mipmap blend"); mods.append("filter")
+    ShadowMapTexture = glLibTexture(None,mods,[ShadowMapSize,ShadowMapSize])
+    if not texturing: glDisable(GL_TEXTURE_2D)
+    return ShadowMapTexture
+
+def initialize_shadowmap(shadowmap,lightpos,lightfocus=(0,0,0),lightviewangle=100,near=0.1,far=100.0,offset=0.5):
+    ShadowMap = ShadowMaps[shadowmap-GLLIB_SHADOW_MAP1]
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    gluPerspective(lightviewangle,1.0,near,far)
+    LightProjectionMatrix = glGetFloatv(GL_PROJECTION_MATRIX)
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+    gluLookAt(lightpos[0],lightpos[1],lightpos[2],
+              lightfocus[0],lightfocus[1],lightfocus[2],
+              0.0,1.0,0.0)
+    LightViewMatrix = glGetFloatv(GL_MODELVIEW_MATRIX)
+    #Use viewport the same size as the shadow map
+    
+    glViewport(0, 0, ShadowMap[1], ShadowMap[1])
+    glPolygonOffset(offset,offset)
+    glEnable(GL_POLYGON_OFFSET_FILL) 
+    #eval projection matrix
+    glMatrixMode(GL_MODELVIEW)
+    glPushMatrix()
+    glLoadMatrixf([[0.5,0.0,0.0,0.0],
+                   [0.0,0.5,0.0,0.0],
+                   [0.0,0.0,0.5,0.0],
+                   [0.5,0.5,0.5,1.0]])
+    glMultMatrixf(LightProjectionMatrix)
+    glMultMatrixf(LightViewMatrix)
+    TextureMatrix = glGetFloatv(GL_TRANSPOSE_MODELVIEW_MATRIX)
+    ShadowMap[2] = TextureMatrix
+    glPopMatrix()
+
+def create_shadowmap(shadowmap):
+    ShadowMap = ShadowMaps[shadowmap-GLLIB_SHADOW_MAP1]
+    #Write texture into texture obj
+    texturing = glGetBooleanv(GL_TEXTURE_2D)
+    glEnable(GL_TEXTURE_2D)
+    glBindTexture(GL_TEXTURE_2D, ShadowMap[0])
+    glCopyTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, 0, 0, ShadowMap[1], ShadowMap[1])
+    glDisable(GL_POLYGON_OFFSET_FILL)
+    if not texturing: glDisable(GL_TEXTURE_2D)
+
+def enable_shadowshading():
+    glBlendFunc(GL_SRC_COLOR,GL_DST_COLOR)
+    glEnable (GL_BLEND)
+
+def disable_shadowshading():
+    glDisable(GL_BLEND)
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+
+def render_shadow_comparison(shadowmap):
+    global Texturing
+    Texturing = glGetBooleanv(GL_TEXTURE_2D)
+    glEnable(GL_TEXTURE_2D)
+    glEnable(GL_TEXTURE_GEN_S)
+    glEnable(GL_TEXTURE_GEN_T)
+    glEnable(GL_TEXTURE_GEN_R)
+    glEnable(GL_TEXTURE_GEN_Q)
+    ShadowMap = ShadowMaps[shadowmap-GLLIB_SHADOW_MAP1]
+    TextureMatrix = ShadowMap[2]
+    #Evaluate where to draw shadows using ARB extension        
+    glBindTexture(GL_TEXTURE_2D, ShadowMap[0])
+    glTexGeni(GL_S, GL_TEXTURE_GEN_MODE, GL_EYE_LINEAR)
+    glTexGenfv(GL_S, GL_EYE_PLANE, TextureMatrix[0])
+    glTexGeni(GL_T, GL_TEXTURE_GEN_MODE, GL_EYE_LINEAR)
+    glTexGenfv(GL_T, GL_EYE_PLANE, TextureMatrix[1])
+    glTexGeni(GL_R, GL_TEXTURE_GEN_MODE, GL_EYE_LINEAR)
+    glTexGenfv(GL_R, GL_EYE_PLANE, TextureMatrix[2])
+    glTexGeni(GL_Q, GL_TEXTURE_GEN_MODE, GL_EYE_LINEAR)
+    glTexGenfv(GL_Q, GL_EYE_PLANE, TextureMatrix[3])
+    #Enable shadow comparison
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_COMPARE_MODE_ARB, GL_COMPARE_R_TO_TEXTURE_ARB)
+    #Shadow comparison should be True (in shadow) if r > texture
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_COMPARE_FUNC_ARB, GL_GREATER)
+    #Shadow comparison should generate an INTENSITY result
+    glTexParameteri(GL_TEXTURE_2D, GL_DEPTH_TEXTURE_MODE_ARB, GL_INTENSITY)
+    #Set alpha test to discard false comparisons
+    glAlphaFunc(GL_EQUAL, 1.0)
+
+def reset_OpenGL_parameters():
+    #reset gl params after comparison
+##    if not Texturing: glDisable(GL_TEXTURE_2D)
+    glDisable(GL_TEXTURE_2D)
+    glDisable(GL_TEXTURE_GEN_S)
+    glDisable(GL_TEXTURE_GEN_T)
+    glDisable(GL_TEXTURE_GEN_R)
+    glDisable(GL_TEXTURE_GEN_Q)
+
+# This part should be moved, is here since I didn't know which parts
+# were needed
+
+def glLibTexturing(value):
+    if value:glEnable(GL_TEXTURE_2D)
+    else:glDisable(GL_TEXTURE_2D)
+def glLibTexture(surface,filters=[],size="automatic"):
+    if type(surface) == type(""):
+        surface = pygame.image.load(os.path.join(*surface.split("/"))).convert_alpha()
+    if surface == None:
+        data = surface
+    else:
+        data = pygame.image.tostring(surface,"RGBA",True)
+    if size == "automatic":
+        width,height = surface.get_size()
+    else:
+        width = size[0]
+        height = size[1]
+    texture = glGenTextures(1)
+    glBindTexture(GL_TEXTURE_2D,texture)
+    #Format
+    InternalFormat = GL_RGBA
+    if "depthtex" in filters:
+        InternalFormat = GL_DEPTH_COMPONENT
+    #Mag filter
+    if "filter" in filters: glTexParameterf(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR)
+    else:                   glTexParameterf(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST)
+    #Min filter
+    if "mipmap" in filters:
+        if "mipmap blend" in filters:
+            if "filter" in filters: mipmap_param = GL_LINEAR_MIPMAP_LINEAR
+            else:                   mipmap_param = GL_NEAREST_MIPMAP_LINEAR
+        else:
+            if "filter" in filters: mipmap_param = GL_LINEAR_MIPMAP_NEAREST
+            else:                   mipmap_param = GL_NEAREST_MIPMAP_NEAREST
+        glTexParameterf(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,mipmap_param)
+        glPixelStoref(GL_UNPACK_ALIGNMENT,1)
+        gluBuild2DMipmaps(GL_TEXTURE_2D,3,width,height,InternalFormat,GL_UNSIGNED_BYTE,data)
+    else:
+        if "filter" in filters: glTexParameterf(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR)
+        else:                   glTexParameterf(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST)
+        glTexImage2D(GL_TEXTURE_2D,0,InternalFormat,width,height,0,InternalFormat,GL_UNSIGNED_BYTE,data)
+    #Misc.
+    if "clamp" in filters:
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP)
+    #Return
+    return texture
+def glLibSelectTexture(texture):
+    glBindTexture(GL_TEXTURE_2D,texture)
